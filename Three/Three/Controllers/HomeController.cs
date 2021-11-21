875cfd771e792657e4d2761de81f2ea664cb1d87
jiao-zi <37893457+jiao-zi@users.noreply.github.com>
2021-11-21 11:12:55 +0800

    Add files via upload

---
+++ Three/Three/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Three.Services;
+
+namespace Three.Controllers
+{
+    public class HomeController : Controller
+    {
+        //DI依赖注入，在StartUp里面注册了实例，注入参数的IClock注册的对应实例ChinaClock的实例
+        public HomeController(IClock clock)
+        { 
+        
+        }
+    }
+}
